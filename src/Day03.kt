0b0b51657e0825d6b4f68a792fedcd1b0e807b93
frungl <fruuungl@gmail.com>
2022-12-03 12:05:44 +0300

    solved day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,16 +1,37 @@
 fun main() {
+    fun cost(ch: Char): Int {
+        return when(ch) {
+            in 'a'..'z' -> ch - 'a' + 1
+            else -> ch - 'A' + 27
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return 0
+        return input.sumOf { str ->
+            val (l, r) = str.chunked(str.length / 2)
+            l.toCharArray().distinct().sumOf { ch ->
+                when (r.contains(ch)) {
+                    true -> cost(ch)
+                    else -> 0
+                }
+            }
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        return input.chunked(3).sumOf { bp ->
+            bp[0].toCharArray().distinct().sumOf { ch ->
+                when (bp[1].contains(ch) && bp[2].contains(ch)) {
+                    true -> cost(ch)
+                    else -> 0
+                }
+            }
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+    check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03")
     println(part1(input))
