b981769e6d405fa7d1d611b1fcfa1a3fcfc611cc
frungl <fruuungl@gmail.com>
2022-12-04 17:07:09 +0300

    solved day 4

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,15 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        return input.count { str ->
+            val (l, r) = str.split(',')
+            val (la, lb) = l.split('-').map { it.toInt() }
+            val (ra, rb) = r.split('-').map { it.toInt() }
+            (la <= ra && rb <= lb) || (ra <= la && lb <= rb)
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        return input.count { str ->
+            val (l, r) = str.split(',')
+            val (la, lb) = l.split('-').map { it.toInt() }
+            val (ra, rb) = r.split('-').map { it.toInt() }
+            !(la > rb || ra > lb)
+        }
     }
 
     val testInput = readInput("Day04_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
     val input = readInput("Day04")
     println(part1(input))
