73fea4b7cbf0211f5919f15d7f245e0e967c8f15
frungl <fruuungl@gmail.com>
2022-12-06 23:43:18 +0300

    add beautiful solution for day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -21,6 +21,15 @@ fun main() {
         }.sorted().takeLast(3).sum()
     }
 
+    fun beauty(input: List<String>): List<Int> =
+        input.joinToString(separator="\n").trim().split("\n\n").map { it.lines().sumOf(String::toInt) }.sortedDescending()
+
+    fun part1Beauty(input: List<String>): Int =
+        beauty(input).first()
+
+    fun part2Beauty(input: List<String>): Int =
+        beauty(input).take(3).sum()
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 24000)
@@ -29,4 +38,6 @@ fun main() {
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+    println(part1Beauty(input))
+    println(part2Beauty(input))
 }
