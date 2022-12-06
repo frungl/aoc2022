f0a406c2d6fc427e0672bfce4cb9ec1fb36b058f
frungl <fruuungl@gmail.com>
2022-12-06 14:19:34 +0300

    solved day 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,15 +1,33 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        val str = input.first()
+        val tmp = str.mapIndexed { index, _ ->
+            when(str.length - index >= 4) {
+                true -> str.substring(index, index + 4)
+                else -> ""
+            }
+        }.dropLast(3)
+        return 4 + tmp.indexOfFirst {
+            it.toCharArray().distinct().size == 4
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val str = input.first()
+        val tmp = str.mapIndexed { index, _ ->
+            when(str.length - index >= 14) {
+                true -> str.substring(index, index + 14)
+                else -> ""
+            }
+        }.dropLast(13)
+        return 14 + tmp.indexOfFirst {
+            it.toCharArray().distinct().size == 14
+        }
     }
 
     val testInput = readInput("Day06_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 19)
 
     val input = readInput("Day06")
     println(part1(input))
